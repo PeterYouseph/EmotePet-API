1ff3a924250c51e63ac32c7974de0e12c8f27614
peteryouseph <peteryouseph@gmail.com>
2024-06-24 18:59:55 -0400

    feat: implementa Bedrock service base teste da aplicação
    Service responsável pelo texto gerado no Bedrock com dicas para os Pets

---
+++ visao-computacional/services/bedrock_service.py
@@ -0,0 +1,8 @@
+
+def get_pet_tips(labels):
+    # Obtendo a lista de rótulos detectados na imagem
+    return ("Dicas sobre Labradores: Nível de Energia e Necessidades de Exercícios: Labradores são de médio nível de energia, "
+            "necessitando de 40 minutos de exercício por dia. Temperamento e Comportamento: Inteligentes, enérgicos, dóceis, "
+            "e com forte desejo de trabalhar com pessoas. Cuidados e Necessidades: Pelos curtos que precisam de poucos cuidados, "
+            "mas devem ser penteados uma vez por semana para remover fios mortos e soltos. A alimentação deve ser adequada, ajustando "
+            "a quantidade conforme o peso do cão. Problemas de Saúde Comuns: Displasia do cotovelo e coxofemoral, atrofia progressiva da retina (APR) e catarata hereditária.")
