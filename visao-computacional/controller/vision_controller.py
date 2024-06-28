f2105183dc527d330780532fea06383cd29ae46b
peteryouseph <peteryouseph@gmail.com>
2024-06-28 09:09:49 -0400

    refactor: modulariza e torna vision_controller uma classe OOP
    refactoring visando melhor modularidade do projeto

--- visao-computacional/controller/vision_controller.py
+++ visao-computacional/controller/vision_controller.py
@@ -1,20 +1,78 @@
-import json
-from services.rekognition_service import detect_faces, detect_pets
-from services.s3_service import get_image_metadata, get_signed_url
-from services.bedrock_service import get_pet_tips
+from services.rekognition_service import RekognitionService
+from services.s3_service import S3Service
+# services.bedrock_service import BedrockService
 from utils.logger import logger, error
 
-# Função para processar uma imagem e detectar faces
-def process_image(bucket, image_name): 
-    try: 
-        # Detectando faces na imagem
-        response_faces = detect_faces(bucket, image_name)
-        # Obtendo metadados da imagem e URL assinada
-        metadata = get_image_metadata(bucket, image_name)
-        image_url = get_signed_url(bucket, image_name)
-        creation_time = metadata['LastModified'].strftime('%d-%m-%Y %H:%M:%S')
+class VisionController:
+    def __init__(self, bucket, image_name):
+        self.bucket = bucket
+        self.image_name = image_name
+        self.rekognition_service = RekognitionService()
+        self.s3_service = S3Service()
+        # self.bedrock_service = BedrockService()
+    
+    # Função para processar uma imagem e detectar faces
+    def process_image(self):
+        try:
+            # Detectando faces na imagem
+            response_faces = self.rekognition_service.detect_faces(self.bucket, self.image_name)
+            # Obtendo metadados da imagem e URL assinada
+            metadata = self.s3_service.get_image_metadata(self.bucket, self.image_name)
+            image_url = self.s3_service.get_signed_url(self.bucket, self.image_name)
+            creation_time = metadata['LastModified'].strftime('%d-%m-%Y %H:%M:%S')
 
-        # Processando as faces detectadas
+            # Processando as faces detectadas
+            faces = self._process_faces(response_faces)
+
+            # Montando a resposta da requisição com os dados processados
+            response_body = {
+                "url_to_image": image_url,
+                "created_image": creation_time,
+                "faces": faces
+            }
+            return response_body
+        # Em caso de erro, loga a exceção e retorna
+        except Exception as e:
+            error(e)
+            raise Exception('Internal Server Error')
+
+    # Função para processar uma imagem e detectar faces e animais de estimação
+    def process_image_with_pets(self):
+        try:
+            # Detectando rótulos na imagem e filtrando animais de estimação
+            pet_labels = self.rekognition_service.detect_pets(self.bucket, self.image_name)
+            rekognition_faces = self.rekognition_service.detect_faces(self.bucket, self.image_name)
+            logger(f'Rekognition data: {pet_labels}')
+
+            # Processando as faces detectadas
+            faces = self._process_faces(rekognition_faces)
+
+            # Obtendo metadados da imagem e URL assinada
+            metadata = self.s3_service.get_image_metadata(self.bucket, self.image_name)
+            image_url = self.s3_service.get_signed_url(self.bucket, self.image_name)
+            creation_time = metadata['LastModified'].strftime('%d-%m-%Y %H:%M:%S')
+
+            response = {
+                'url_to_image': image_url,
+                'created_image': creation_time,
+                'faces': faces
+            }
+
+            # Adicionar informações de pets caso tenha detectado algum
+            if pet_labels:
+                pets = self._process_pets(pet_labels)
+                response['pets'] = pets
+                print(f'Pets encontrados: {pets}')
+            else:
+                response['pets'] = []
+
+            return response
+        # Em caso de erro, loga a exceção e retorna
+        except Exception as e:
+            error(e)
+            raise Exception('Internal Server Error')
+
+    def _process_faces(self, response_faces):
         faces = []
         if 'FaceDetails' in response_faces:
             for faceDetail in response_faces['FaceDetails']:
@@ -29,7 +87,6 @@ def process_image(bucket, image_name):
                     "classified_emotion_confidence": float(faceDetail["Emotions"][0]["Confidence"])
                 }
                 faces.append(face)
-        # Se não houver faces detectadas, retorna um objeto com valores nulos
         if not faces:
             faces = [{
                 "position": {
@@ -41,81 +98,17 @@ def process_image(bucket, image_name):
                 "classified_emotion": None,
                 "classified_emotion_confidence": None
             }]
-        # Montando a resposta da requisição com os dados processados
-        response_body = {
-            "url_to_image": image_url,
-            "created_image": creation_time,
-            "faces": faces
-        }
+        return faces
 
-        return response_body
-    # Em caso de erro, loga a exceção e retorna
-    except Exception as e:
-        error(e)
-        raise Exception('Internal Server Error')
-
-# Função para processar uma imagem e detectar faces e animais de estimação
-def process_image_with_pets(bucket, image_name):
-    try:
-        # Detectando rótulos na imagem e filtrando animais de estimação
-        pet_labels = detect_pets(bucket, image_name)
-        rekognition_faces = detect_faces(bucket, image_name)
-        logger(f'Rekognition data: {pet_labels}')
-
-        # Processando as faces detectadas
-        faces = []
-        if 'FaceDetails' in rekognition_faces:
-            for face in rekognition_faces['FaceDetails']:
-                emotion = max(face['Emotions'], key=lambda x: x['Confidence'])
-                faces.append({
-                    'position': {
-                        "Height": float(face['BoundingBox']['Height']),
-                        "Left": float(face['BoundingBox']['Left']),
-                        "Top": float(face['BoundingBox']['Top']),
-                        "Width": float(face['BoundingBox']['Width'])
-                    },
-                    'classified_emotion': emotion['Type'],
-                    'classified_emotion_confidence': float(emotion['Confidence'])
-                })
-
-        # Obtendo metadados da imagem e URL assinada
-        metadata = get_image_metadata(bucket, image_name)
-        image_url = get_signed_url(bucket, image_name)
-        creation_time = metadata['LastModified'].strftime('%d-%m-%Y %H:%M:%S')
-
-        response = {
-            'url_to_image': image_url,
-            'created_image': creation_time,
-            'faces': faces
-        }
-        # Adicionar informações de faces caso não tenha detectado nenhuma
-        if not faces:
-            response['faces'] = [{
-                "position": {
-                    "Height": None,
-                    "Left": None,
-                    "Top": None,
-                    "Width": None
-                },
-                "classified_emotion": None,
-                "classified_emotion_confidence": None
-            }]
-
-        # Adicionar informações de pets caso tenha detectado algum
-        if pet_labels:
-            # pet_tips = get_pet_tips(image_url, [label['type'] for label in pet_labels if label['type']])
-            response['pets'] = [{
-                'labels': [[{'Confidence': label['confidence'],
-                             'Name': label['type']} for label in pet_labels if label['type']],
-                           {'Confidence': breed['confidence'],
-                            'Name': breed['breed']} for label in pet_labels for breed in label['breeds']],
-                'Dicas': 'pet_tips'
-            }] # Formato da resposta com 
-        else:
-            response['pets'] = []
-
-        return response
-    # Em caso de erro, loga a exceção e retorna
-    except Exception as e:
-        error(e)
-        raise Exception('Internal Server Error')
+    def _process_pets(self, pet_labels):
+        # Remover duplicatas de rótulos e raças de animais de estimação
+        unique_breeds = {breed['breed']: breed for label in pet_labels for breed in label['breeds']}.values()
+        
+        # Formatar as dicas de animais de estimação para a resposta - TO DO
+        #pet_tips = self.bedrock_service()
+        
+        pets = [{
+            'labels': [{'Confidence': breed['confidence'], 'Name': breed['breed']} for breed in unique_breeds],
+            'Dicas': 'pet_tips'
+        }]
+        return pets
