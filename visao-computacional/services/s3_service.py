74b9e3f877338b0aca91f744ea01ee977b7d1c23
peteryouseph <peteryouseph@gmail.com>
2024-06-28 09:12:12 -0400

    refactor: modulariza e torna s3Service uma classe OOP
    refactoring visando melhor modularidade do projeto

--- visao-computacional/services/s3_service.py
+++ visao-computacional/services/s3_service.py
@@ -1,13 +1,17 @@
 import boto3
 
-s3 = boto3.client('s3')
+class S3Service:
+    def __init__(self):
+        self.s3 = boto3.client('s3')
 
-def get_image_metadata(bucket, image_name): # Função para obter os metadados de uma imagem no S3 e retornar o objeto de metadados
-    metadata = s3.head_object(Bucket=bucket, Key=image_name)
-    return metadata
+    # Função para obter os metadados de uma imagem no S3 e retornar o objeto de metadados
+    def get_image_metadata(self, bucket, image_name):
+        metadata = self.s3.head_object(Bucket=bucket, Key=image_name)
+        return metadata
 
-def get_signed_url(bucket, image_name): # Função para gerar uma URL assinada para acessar a imagem no S3
-    url = s3.generate_presigned_url('get_object',
-                                    Params={'Bucket': bucket, 'Key': image_name},
-                                    ExpiresIn=3600)
-    return url
+    # Função para gerar uma URL para acessar a imagem no S3
+    def get_signed_url(self, bucket, image_name):
+        url = self.s3.generate_presigned_url('get_object',
+                                             Params={'Bucket': bucket, 'Key': image_name},
+                                             ExpiresIn=3600)
+        return url
